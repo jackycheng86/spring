869c12a7e2f7800eda1b238a837ffc6eb04d1ba5
jacky <cj@sicnu.edu.cn>
2019-07-13 23:27:18 +0800

    add spring security demo

---
+++ spring-boot-security/src/main/java/com/spring/security/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.spring.security;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootSecurityApplication.class);
+    }
+
+}
