28097db2b3973b133c3db69b26918be8a124d156
chengjian <cj@sicnu.edu.cn>
2019-09-29 08:00:49 +0800

    完善MethodInvokeJob

--- spring-boot-quartz/src/main/java/com/spring/quartz/schedule/methodinovke/MethodJob1.java
+++ spring-boot-quartz/src/main/java/com/spring/quartz/schedule/methodinovke/MethodJob1.java
@@ -1,10 +1,27 @@
 package com.spring.quartz.schedule.methodinovke;
 
+import com.spring.quartz.book.entity.Book;
+import com.spring.quartz.book.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MethodJob1 {
-    public void doJob(){
+    private BookService bookService;
 
+    @Autowired
+    public MethodJob1(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    public void doJob(){
+        try {
+            System.out.println("------------------任务开始MethodJob1------------------");
+            Book book = bookService.add();
+            System.out.println(book.toString());
+            System.out.println("------------------任务结束MethodJob1------------------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- spring-boot-quartz/src/main/java/com/spring/quartz/schedule/methodinovke/MethodJob2.java
+++ spring-boot-quartz/src/main/java/com/spring/quartz/schedule/methodinovke/MethodJob2.java
@@ -1,10 +1,28 @@
 package com.spring.quartz.schedule.methodinovke;
 
+import com.spring.quartz.book.entity.Book;
+import com.spring.quartz.book.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MethodJob2 {
-    public void doJob(){
 
+    private BookService bookService;
+
+    @Autowired
+    public MethodJob2(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    public void doJob(){
+        try {
+            System.out.println("------------------任务开始MethodJob2------------------");
+            Book book = bookService.get();
+            System.out.println(book.toString());
+            System.out.println("------------------任务结束MethodJob2------------------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
